1feab6f6fc85959179730b7ffa267f59776b6ada
Arman Kolahan <akolahan@gmail.com>
2022-12-07 16:33:45 +0100

    feat/firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../theme/theme";
+import { FirebaseProvider } from "../features/firebase/firebase.provider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <FirebaseProvider>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </FirebaseProvider>
   );
 }
