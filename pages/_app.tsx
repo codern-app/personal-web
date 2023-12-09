72375b782eed965591ac44a2a3c67172230de04e
Arman Kolahan <akolahan@gmail.com>
2023-12-09 18:43:26 +0100

    fix: update intlprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,17 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '../theme/theme';
 import { FirebaseProvider } from '../features/firebase/providers/firebase.provider';
-import { NextIntlProvider } from 'next-intl';
+import { IntlProvider } from 'next-intl';
 import en from '../i18n/en.json';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <FirebaseProvider>
-      <NextIntlProvider messages={pageProps.messages || en} locale="en-US">
+      <IntlProvider messages={pageProps.messages || en} locale="en-US">
         <ThemeProvider theme={theme}>
           <Component {...pageProps} />
         </ThemeProvider>
-      </NextIntlProvider>
+      </IntlProvider>
     </FirebaseProvider>
   );
 }
